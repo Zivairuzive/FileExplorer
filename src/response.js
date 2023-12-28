4670d21b72e51149adc676cf29ac7ad82c091e73
ZivaiRuzive <zivaiherbert@gmail.com>
2023-12-28 16:28:08 +0200

    captured request pathname

--- src/response.js
+++ src/response.js
@@ -1,7 +1,25 @@
+let url = require("url")
+
+var decode_url, build_path='';
+
+
+function _replace(pattern, string, replacement){
+    var re = new RegExp(pattern, 'g')
+    return string.replace(re,replacement)
+}
 
 const handler = (request, response)=>{
-    console.log(request);
     response.writeHead(200, {"Content-Type":"application/json"});
+    let pathname = request.url;
+
+    // exclude favicon ico from the returned path
+    if (pathname == '/favicon.ico'){
+        return false }
+        
+    // uriComponent replace with space 
+    // wrapper _replace remove those spaces
+    decode_url = _replace(' ',decodeURIComponent(pathname),'');
+
     response.end(JSON.stringify({
         data: "Hello World",
     }));
