413e581deb158c66f99ad1c2d96fd3c48643d32d
ZivaiRuzive <zivaiherbert@gmail.com>
2023-12-28 19:31:43 +0200

    Add frontend files

---
+++ src/constant.js
@@ -0,0 +1,7 @@
+const SPACE = ' ';
+const FAVICON = '/favicon.ico';
+const NO_SPACE = '';
+const STATIC_FOLDER_NAME = 'static';
+
+
+module.exports = {SPACE, FAVICON, NO_SPACE, STATIC_FOLDER_NAME};
--- src/response.js
+++ src/response.js
@@ -1,7 +1,11 @@
 let url = require("url")
+const {SPACE, FAVICON, NO_SPACE, STATIC_FOLDER_NAME} = require('./constant');
+const path = require("path");
+const fs = require("fs");
 
-var decode_url, build_path='';
 
+
+var decode_url;
 
 function _replace(pattern, string, replacement){
     var re = new RegExp(pattern, 'g')
@@ -9,20 +13,47 @@ function _replace(pattern, string, replacement){
 }
 
 const handler = (request, response)=>{
-    response.writeHead(200, {"Content-Type":"application/json"});
+    response.writeHead(200, {"Content-Type":"text/html"});
     let pathname = request.url;
 
+    //static BaseFile for file on the server
+    const BaseStaticPath = path.join(__dirname,'..',STATIC_FOLDER_NAME);
+
+
     // exclude favicon ico from the returned path
-    if (pathname == '/favicon.ico'){
+    if (pathname == FAVICON){
         return false }
-        
+
     // uriComponent replace with space 
     // wrapper _replace remove those spaces
-    decode_url = _replace(' ',decodeURIComponent(pathname),'');
+    decode_url = _replace(SPACE,decodeURIComponent(pathname),NO_SPACE);
+    // full static file rom url; 
+    const FullPathName = path.join(BaseStaticPath, decode_url);
+    // check if exist 
+    let stat;
+    if(!fs.existsSync(FullPathName)){
+        response.write('404: File Not Found\n')
+        response.end()
+        return false;
+        
+    }else{
+        try{
+            stat = fs.lstatSync(FullPathName)
+        }catch(er){
+            console.log(`lstatSync Error:  ${err}`)
+        }
+    }
 
-    response.end(JSON.stringify({
-        data: "Hello World",
-    }));
+    // check if path is a directory
+    if(stat.isDirectory()){
+        let data = fs.readFileSync(
+            path.join(
+                BaseStaticPath, 'project_files/index.html')
+                , 'utf-8')
+        response.statusCode = 200;
+        response.write(data);
+        response.end()
+    }
 }
 
 
